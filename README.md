e251a382d8582e140d80c0663050d137a575b5e6
“Zaria.Levi” <zarialevi@gmail.com>
2019-11-01 08:30:30 +0000

    updates

--- README.md
+++ README.md
@@ -14,10 +14,10 @@ Modelled on a simple [Kanban](https://www.atlassian.com/agile/kanban) process wo
 ||||2|
 ||||3|
 ||||4|
-|5||||
-|6||||
-|7||||
-|8||||
+||||5|
+||||6|
+||7|||
+||8|||
 |9||||
 |10||||
 |11||||
--- lib.py
+++ lib.py
@@ -1,5 +1,17 @@
 # You don't have to use these classes, but we recommend them as a good place to start!
 
+from dotenv import load_dotenv
+import os
+load_dotenv()
+class WeatherGetter():
+    def __init__(self):
+        # Let's set our secrets and keys from the .env file
+        # as environment variables.
+        self.BASE_URL = 'https://api.darksky.net'
+        self.token = os.getenv('DARKSKY_KEY')
+        if len(self.token) == 0:
+            raise ValueError('Missing API key!')
+
 class MongoHandler():
     pass
 
